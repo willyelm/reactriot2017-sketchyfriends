251fdde0b6053fbad2ee178a3da45a63782f3f9c
Te Vallee <astronomemusic@gmail.com>
2017-06-23 15:06:21 -1000

    added input & submit button

--- src/Game.js
+++ src/Game.js
@@ -2,11 +2,35 @@ import React, { Component } from 'react';
 import Canvas from './Canvas';
 
 class Game extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      input: '',
+      guess: ''
+    }
+  }
+
+  handleChange(e) {
+    this.setState({ input: e.target.value });
+  }
+
+  handleClick() {
+    this.setState({
+      guess: this.state.input,
+      input: ''
+    });
+  }
+
   render() {
     return (
       <div className="Game">
         <h1>Sketchy Friends</h1>
         <Canvas />
+        <p>{ this.state.guess }</p>
+        <input type="text" onChange={ this.handleChange.bind(this) } value={ this.state.input } />
+        <button onClick={ this.handleClick.bind(this) } type="button">Submit</button>
       </div>
     );
   }
