c8034df946423f6949019c5867709221eb5aa3e4
Te Vallee <astronomemusic@gmail.com>
2017-06-23 14:37:34 -1000

    react-sketchpad added/implemented

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,10 +1,59 @@
 import React, { Component } from 'react';
+import { SketchPad, TOOL_PENCIL, TOOL_LINE, TOOL_RECTANGLE, TOOL_ELLIPSE } from '../node_modules/react-sketchpad/lib/index';
 
 class Canvas extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tool: TOOL_PENCIL,
+      size: 2,
+      color: '#000000',
+      fill: false,
+      fillColor: '#444444',
+      items: []
+    }
+  }
+
   render() {
+    const { tool, size, color, fill, fillColor, items } = this.state;
+
     return (
       <div className="Canvas">
-        <h1>Canvas Here</h1>
+        <SketchPad
+          width={500}
+          height={500}
+          animate={true}
+          size={size}
+          color={color}
+          fillColor={fill ? fillColor : ''}
+          items={items}
+          tool={tool}
+        />
+        <div style={{float:'left'}}>
+          <div className="tools" style={{marginBottom:20}}>
+            <button
+              style={tool == TOOL_PENCIL ? {fontWeight:'bold'} : undefined}
+              className={tool == TOOL_PENCIL  ? 'item-active' : 'item'}
+              onClick={() => this.setState({tool:TOOL_PENCIL})}
+            >Pencil</button>
+          </div>
+          <div className="options" style={{marginBottom:20}}>
+            <label htmlFor="">color: </label>
+            <input type="color" value={color} onChange={(e) => this.setState({color: e.target.value})} />
+          </div>
+          {(this.state.tool == TOOL_ELLIPSE || this.state.tool == TOOL_RECTANGLE) ?
+            <div>
+              <label htmlFor="">fill in:</label>
+              <input type="checkbox" value={fill} style={{margin:'0 8'}}
+                     onChange={(e) => this.setState({fill: e.target.checked})} />
+              {fill ? <span>
+                  <label htmlFor="">with color:</label>
+                  <input type="color" value={fillColor} onChange={(e) => this.setState({fillColor: e.target.value})} />
+                </span> : ''}
+            </div> : ''}
+        </div>
       </div>
     );
   }
